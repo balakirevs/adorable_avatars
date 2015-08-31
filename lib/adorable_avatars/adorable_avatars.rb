e2c61e8a937f14ca8c54162ff78a1d98b2779092
mswiszcz <swistaku+mail@gmail.com>
2015-08-31 14:31:41 +0200

    add methods to create adorable_avatars

--- lib/adorable_avatars/adorable_avatars.rb
+++ lib/adorable_avatars/adorable_avatars.rb
@@ -1,3 +1,21 @@
 module AdorableAvatars
+  mattr_accessor :default_size
 
+  @@default_size = 150
+
+  def adorable_avatar(digest, options = {})
+    size = options.delete(:size) || default_size
+
+    image_tag adorable_avatar_url(digest, size), **options
+  end
+
+  def adorable_avatar_url(digest, size)
+    "http://api.adorable.io/avatars/#{size}/#{digest}.png"
+  end
+
+  def self.setup
+    yield self if block_given?
+  end
 end
+
+ActionView::Base.send :include, AdorableAvatars
